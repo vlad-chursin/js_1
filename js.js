185ecedf77f40384931a67dea7b23a45e84dae01
vlad-chursin <vachursin@edu.hse.ru>
2025-01-21 08:59:51 +0900

    tasks

---
+++ js.js
@@ -0,0 +1,16 @@
+// Задание №1
+var str = 'abcde';
+console.log(str[0], str[1], str[4]);
+
+// Задание №2
+let a = parseInt(prompt("введи первое число, брат: "));
+let b = parseInt(prompt("введи второго число, брат:"));
+console.log(a > 0 || b > 0);
+
+// Задание №3
+let age = parseInt(prompt("чувак, введи сколько годиков:"));
+if (age >= 18) {
+    console.log("Совершеннолетний");
+} else {
+    console.log("Несовершеннолетний");
+}
